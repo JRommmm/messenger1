55b4b7e39d8aa0997c67e422fedc7e3482a7ebe6
Valerie Ingmann <valerieingmann707@gmail.com>
2020-12-11 20:37:47 -0500

    fix: removed login import from component folder

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,5 @@
 export { default as Home } from "./Home";
-export { default as Login } from "./Login";
-export { default as LoginForm } from "./LoginForm";
 export { default as Auth } from "./Auth";
+export { default as LoginForm } from "./LoginForm";
 export { default as RegisterForm } from "./RegisterForm";
 export { default as SnackbarError } from "./SnackbarError";
