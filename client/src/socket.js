438e7bf588f8b505824fd4ee6edf944cf6a26028
Valerie Ingmann <valerieingmann707@gmail.com>
2020-12-17 12:36:35 -0500

    feat: connected sockets

---
+++ client/src/socket.js
@@ -0,0 +1,16 @@
+import io from "socket.io-client";
+import store from "./store";
+import { setNewMessage } from "../src/store/conversations";
+
+const socket = io(window.location.origin);
+
+socket.on("connect", () => {
+  console.log("connected to server");
+
+  // socket.on("new-message", (message) => {
+  //   console.log("client new message", message);
+  //   store.dispatch(setNewMessage(message));
+  // });
+});
+
+export default socket;
